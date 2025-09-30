4a605f480f8b3d8b11ec1687ff3ca936673ec910
yaroslav_polishchuk <y.polishchuk05@gmail.com>
2025-09-30 21:53:53 +0200

    Початкове налаштування TypeScript проєкту

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let username: string = "Ярослав";
+let age: number = 21;
+let isStudent: boolean = true;
+
+console.log(`Ім'я: ${username}`);
+console.log(`Вік: ${age}`);
+console.log(`Студент: ${isStudent}`);
