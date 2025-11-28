eb5ce7bd063a5097c178e7f6cbee3245bc43bba9
yaroslav_polishchuk <y.polishchuk05@gmail.com>
2025-11-28 15:19:17 +0100

    Add HTML/CSS template and TypeScript interactivity

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-let username: string = "Ярослав";
-let age: number = 21;
-let isStudent: boolean = true;
-
-console.log(`Ім'я: ${username}`);
-console.log(`Вік: ${age}`);
-console.log(`Студент: ${isStudent}`);
